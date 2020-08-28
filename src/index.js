f45089fa8c65829f48a8b43c98b857a1fe1904e3
skipware-oz <skipware@bigpond.net.au>
2020-08-28 14:56:58 +1000

    Added extra tasks

--- src/index.js
+++ src/index.js
@@ -24,8 +24,10 @@ import './index.css';
 // }
 
 function Square(props){
+  let className ="square";
+  if (props.winner) className="square winnerSquare";
   return (
-    <button className="square" onClick={props.onClick}>
+    <button className={className} onClick={props.onClick}>
       {props.value}
     </button>
   )
@@ -49,10 +51,14 @@ class Board extends React.Component {
   //   }); // setState will rerender the children Squares. (controlled component)
   // }
   renderSquare(i) {
+    let winner=false;
+    let vector=this.props.winVector;  // we inherit this prop from the Board class.
+    if (vector && vector.indexOf(i) > -1) winner=true;
     return <Square 
               value={this.props.squares[i]} 
               // onClick={() => this.handleClick(i)}  // this points Square class back to Board class handleClick()
               onClick={() => this.props.onClick(i)}  // this points Square class back to Game class handleClick()
+              winner={winner}
             />;
   }
 
@@ -67,25 +73,41 @@ class Board extends React.Component {
     }
     */
 
+    let squares =[], row=[];
+    let sq = 0;
+
+    for (let i=0; i < 3; i++) {
+      row=[];
+      for (let j=0; j < 3; j++) {
+        row.push(this.renderSquare(sq));
+        sq++;
+      }
+      squares.push(<div key={sq} className="board-row">{row}</div>)
+    }
     return (
       <div>
         {/* This is how you add comments into html in JSX */}
+        {console.log("%cThis is how you add browser console logs in JSX html","color:#f0c002")}
         {/* <div className="status">{status}</div> */}
+
+        {/*
         <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+          {this.renderSquare(0, this.props.winVector)}
+          {this.renderSquare(1, this.props.winVector)}
+          {this.renderSquare(2, this.props.winVector)}
         </div>
         <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
+          {this.renderSquare(3, this.props.winVector)}
+          {this.renderSquare(4, this.props.winVector)}
+          {this.renderSquare(5, this.props.winVector)}
         </div>
         <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
+          {this.renderSquare(6, this.props.winVector)}
+          {this.renderSquare(7, this.props.winVector)}
+          {this.renderSquare(8, this.props.winVector)}
         </div>
+        */}
+        {squares}
       </div>
     );
   }
@@ -101,7 +123,7 @@ class Game extends React.Component {
       }],
       stepNumber: 0,
       xIsNext: true,
-      listOrder: true,
+      ascMoveOrder: true,
     };
   }
 
@@ -125,7 +147,7 @@ class Game extends React.Component {
     }); // setState will rerender the children Squares. (controlled component)
   }
 
-  jumpTo(step){
+  jumpTo = step => {
     this.setState({
       stepNumber: step,
       xIsNext: (step % 2)  === 0
@@ -135,50 +157,64 @@ class Game extends React.Component {
   listSort(){
     // Todo
     this.setState({
-      listOrder: !this.state.listOrder,
+      ascMoveOrder: !this.state.ascMoveOrder,
     })
   }
 
+
   render() {
     const history=this.state.history;
-    const current=history[this.state.stepNumber];
-    const winner=calculateWinner(current.squares);
-    const listOrder=this.state.listOrder;
+    const stepNumber=this.state.stepNumber;
+    const current=history[stepNumber];
+    const gameState=calculateWinner(current.squares);
+    const ascMoveOrder=this.state.ascMoveOrder;
+    
 
-    let moves=history.map((step,move) => {
+    let moves=history.map((game,move) => {
       // const rowCol = {(step.lastMove)=>/*mm*/}
       // alert(`lastMove=${move?Math.floor(step.lastMove/3)+', '+step.lastMove%3:'n/a'}`);
+      let classOpt=(stepNumber===move?"selected":"");
       const desc = move ?
       // using template string format (back tick execution :
-        `Go to move #${move} (${Math.floor(step.lastMove/3)}, ${step.lastMove%3})` :
+        `Go to move #${move} (${Math.floor(game.lastMove/3)}, ${game.lastMove%3})` :
         'Go to game start';
       return (
-        <li key={move}>
+        <li key={move} className={classOpt}>
           <button onClick={() => this.jumpTo(move)}>{desc}</button>
         </li>
       )
     })
-    if (!this.state.listOrder) moves=moves.reverse();
-
-    let status
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next Player: ' + (this.state.xIsNext ? 'X' : 'O');
+    if (!this.state.ascMoveOrder) {
+      // moves=moves.reverse(); // this works as well
+      moves.sort((a,b) => {
+        return b.key - a.key    // although docs say you can't access the element key, this works (<li key=..>)
+      })
     }
-  
+    let status="";
+    let vector=null;
+
+    if (!gameState) {
+      status = 'Next Player: ' + (this.state.xIsNext ? 'X' : 'O');
+    } else if (gameState.result==="win") {
+      status = `Winner: player ${gameState.player}`;
+      vector = gameState.vector;
+    } else if (gameState.result==="draw") {
+      status = "Game drawn";
+    }
+
     return (
       <div className="game">
         <div className="game-board">
           <Board 
             squares={current.squares}
             onClick={(i) => this.handleClick(i)}
+            winVector={vector}
             />
         </div>
         <div className="game-info">
           <div>{status}</div>
           <br></br>
-          <button onClick={() => this.listSort()}>step order {listOrder?'(desc)':'(asc)'}</button>
+          <button onClick={() => this.listSort()}>move order {ascMoveOrder?'(desc)':'(asc)'}</button>
           <ol>{moves}</ol>
         </div>
       </div>
@@ -204,11 +240,18 @@ function calculateWinner(squares) {
     [0, 4, 8],
     [2, 4, 6],
   ];
+
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return {result: "win", vector: lines[i], player:squares[a]};
     }
+  }
+  let draw;
+  draw=squares.filter(sq=>sq===null);
+  if (draw.length === 0) {
+    console.log("game drawn") // this is the web browser console !! :-)
+    return {result:"draw",vector:null, player:null};
   }
   return null;
 }
